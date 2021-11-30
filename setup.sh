bcab086b2c34322378cf4ecbbae40ad33a5f9e55
Max Lin <aigooey@gmail.com>
2021-11-30 03:17:01 -0600

    Remove unnecessary npm install.

--- setup.sh
+++ setup.sh
@@ -5,8 +5,6 @@ apt-get -y install bash
 
 curl -sL https://deb.nodesource.com/setup_16.x | bash -
 apt install -y nodejs
-apt install -y npm
-npm install npm@latest -g
 
 cd /autograder/source
 npm install --only=prod
