1223d80de632f992ee5219d9662db027d2b2b9a5
Max Lin <aigooey@gmail.com>
2021-12-02 10:03:46 -0600

    Use apt-get instead of apt.

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 apt-get update
 
-apt-get -y install bash
+apt-get install -y bash
 
 curl -sL https://deb.nodesource.com/setup_16.x | bash -
-apt install -y nodejs
+apt-get install -y nodejs
 
 cd /autograder/source
 npm install --only=prod
