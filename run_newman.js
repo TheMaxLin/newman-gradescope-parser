60cd881a979f4dff5819c35040e42cce4a0b2f63
Max Lin <aigooey@gmail.com>
2021-12-15 19:24:54 -0600

    Add pre-run file checking. Remove redundant test output data.

--- run_newman.js
+++ run_newman.js
@@ -1,12 +1,20 @@
 import { run } from 'newman';
-import { RUN_OPTIONS } from './lib/config.js';
+import { RUN_OPTIONS, REQUIRED_FILES } from './lib/config.js';
+import { checkSubmittedFiles, writeMissingFiles } from './lib/files.js';
 import newmanToGradescope from './lib/parser.js';
 
-// Run the collection in Newman
-// Change run options in lib/config.js
-run(RUN_OPTIONS, (err, summary) => {
-  if (err) { throw err; }
+const missingFiles = checkSubmittedFiles(REQUIRED_FILES);
 
-  // Parse results to Gradescope output
-  newmanToGradescope(summary);
-});
+// Make sure all required files are submitted
+if (missingFiles.length) {
+  writeMissingFiles(missingFiles);
+} else {
+  // Run the collection in Newman
+  // Change run options in lib/config.js
+  run(RUN_OPTIONS, (err, summary) => {
+    if (err) { throw err; }
+
+    // Parse results to Gradescope output
+    newmanToGradescope(summary);
+  });
+}
